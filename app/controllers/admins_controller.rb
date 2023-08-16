a3b2a0aaa7c2ff017e9071fa2e901f41c01456f8
irriz-jayy <jayfelix789@gmail.com>
2023-08-16 12:22:39 +0300

    admin functions added

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -17,15 +17,29 @@ class AdminsController < ApplicationController
     # POST /admins
     def create
       @admin = Admin.new(admin_params)
-    #   byebug
   
         if @admin.save
           render json:@admin, status: :created
         else
-          render json: { errors: @admin.errors.full_messages }, status: :unprocessable_entity
+          render json: { error: @admin.errors.full_messages }, status: :unprocessable_entity
         end
     end
-
+  
+    # PATCH/PUT /admins/1
+    def update
+        if @admin.update(admin_params)
+          render json:@admin, status: :ok
+        else
+          render json: { error: @admin.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+  
+    # DELETE /admins/1
+    def destroy
+      @admin.destroy
+      head :no_content
+    end
+  
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_admin
